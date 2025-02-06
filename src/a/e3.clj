a8b83eeda26dc7ad5f3a660ebada93ecd049c0db
Emanuele <egalli64@gmail.com>
2025-02-06 18:21:06 +0100

    functions

---
+++ src/a/e3.clj
@@ -0,0 +1,9 @@
+;;; Using functions: +, *, first
+(ns a.e3)
+
+;; return the sum of the values
+(defn -main []
+	(println "Adding values:" (+ 8 14 20))
+	(println "Multiplying values:" (* 2 7 3))
+	(println "Getting the first value:" (first [4 5 3 2]))
+)
