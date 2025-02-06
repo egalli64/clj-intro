ee3e48c7bd5e6208f12389896d1944b158ceed56
Emanuele <egalli64@gmail.com>
2025-02-06 17:52:50 +0100

    a script

---
+++ src/a_intro/e1.clj
@@ -0,0 +1,5 @@
+;;; Running a Clojure script
+;;; From CLI, top folder in the repository, assuming src as source root: clj -M src\a_intro\e1.clj
+;; -M tells Clojure _not_ to start the REPL to run the script
+
+(println "Hello, World!")
