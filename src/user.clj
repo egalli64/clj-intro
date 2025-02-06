1bcf966c78e97d55c33b63fa78bf312c3fb98ed1
Emanuele <egalli64@gmail.com>
2025-02-06 17:47:56 +0100

    User startup script

---
+++ src/user.clj
@@ -0,0 +1,6 @@
+;;; User startup script - automatically loaded when the REPL starts
+;; To start the REPL loading also the default dependencies: clj
+;; To start the REPL running just this script: clj -M
+
+;; Uncomment next line to welcome the user
+;; (print "Welcome to ")
