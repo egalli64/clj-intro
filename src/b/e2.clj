f0bdeebfcce0df3ccd5334a617e6191901218d81
Emanuele <egalli64@gmail.com>
2025-02-07 09:42:42 +0100

    clarify

--- src/b/e2.clj
+++ src/b/e2.clj
@@ -1,8 +1,12 @@
-;;; control flow: if
+;;; control flow: _if_ expression
+;;
+;; it follows this structure: (if condition form-1 form-2)
+;; The first form is evaluated if condition is truthy (not nil or false)
+;; The second form is evaluated if condition is falsy (nil or false)
 
 (ns b.e2)
 
 (defn -main [] 
-    (println (if true "That's true" "That's false"))
-    (println (if false "If false you'll get nil!"))
+    (println (if true "That's truthy" "That's falsey"))
+    (println (if false "On falsey the if-expression returns nil!"))
 )
