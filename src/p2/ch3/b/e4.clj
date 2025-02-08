4769149bf818b8120218c5442e13aa4d1038a4df
Emanuele <egalli64@gmail.com>
2025-02-08 16:22:55 +0100

    clarify

--- src/p2/ch3/b/e4.clj
+++ src/p2/ch3/b/e4.clj
@@ -3,7 +3,7 @@
 (ns p2.ch3.b.e4)
 
 (defn -main []
-    ;; using _list literal_ to create vector objects
+    ;; using _list literal_ to create list objects
     (println "An empty list:" '())
     (println "A list with three items:" '(1 2 3))
 
@@ -15,7 +15,7 @@
     (println "Get the first value ..." (nth '(1 2 3) 0))
     (println "Get the last value ..." (nth '(1 2 3) 2))
 
-    ;; adding more elements to a vector by _conj function_ put them at the beginning (for performance reasons)
+    ;; adding more elements to a list by _conj function_ put them at the beginning (for performance reasons)
     (println "Adding an item ..." (conj '(1 2 3) 4))
     (println "Adding more items ..." (conj '(1 2 3) 4 5))
     (println "Adding a list as a _single_ new item ..." (conj '(1 2 3) '(4 5 6)))
