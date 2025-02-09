86c47487fbd2e635f1d5854707cc7290f9a05651
Emanuele <egalli64@gmail.com>
2025-02-09 20:50:04 +0100

    return value

--- src/p2/ch3/c/e3.clj
+++ src/p2/ch3/c/e3.clj
@@ -1,17 +1,24 @@
 ;;; Function definition - in the global space at namespace level
 ;;
 ;; defn function-name "optional docstring" [list of parameters] function_body
+;; the last form evaluated is implicitly returned to the caller
 
 (ns p2.ch3.c.e3)
 
-;; my-hello expects a single argument
-(defn my-hello "A function defined in the current namespace" [name]
-    (println "Hello," name)
-)
+;; hello expects a single argument, the last form is evaluated to nil
+(defn hello "A function defined in the current namespace" [name] (println "Hello," name))
+
+;; get in a value (a number), return a string
+(defn check-negative "A function returning a string" [x] (if (< x 0) "negative" "non negative"))
 
 (defn -main "The main function for the current namespace" []
-    (my-hello "Tom")
+    ;; my hello returns nil
+    (println (hello "Tom"))
+
     ;; won't work: ArityException
-    ;; (my-hello)
-    ;; (my-hello "Tom" "Bob")
+    ;; (hello)
+    ;; (hello "Tom" "Bob")
+
+    (println (check-negative -23))
+    (println (check-negative 42))
 )
