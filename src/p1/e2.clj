dd7b9bc615456e8ce3edea76a7f4471be612a745
Emanuele <egalli64@gmail.com>
2025-04-05 10:40:33 +0200

    review

--- src/p1/e2.clj
+++ src/p1/e2.clj
@@ -1,10 +1,12 @@
 ;;; Running a Clojure app (a script with an entry point: -main)
 ;;; From CLI (top folder in the repository - assuming src as source folder): clj -M -m p1.e2
+;; by default src/ is in the classpath - check it by running: clj -Spath
+;;
 ;; -M tells Clojure _not_ to start the REPL to run the script
 ;; -m tells Closure the namespace that contains the -main to be executed
 ;; notice that in Clojure the program entry point is called -main (to avoid conflicts with Java main)
 ;
-;;; Or, REPL: clj -M
+;;; Or, start the script runner: clj -M
 ;; user=> (require 'p1.e2)
 ;; user=> (in-ns 'p1.e2)
 ;; user=> (-main) 
