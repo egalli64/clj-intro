24938b5d6c41193d30ed31c44018241841530022
Emanuele <egalli64@gmail.com>
2025-04-07 11:25:48 +0200

    read-string

---
+++ src/p2/ch7/b/e1.clj
@@ -0,0 +1,13 @@
+;;; read-string: convert a string to Clojure form
+(ns p2.ch7.b.e1)
+
+(defn -main []
+    ;; read a string, generate a list
+    (let [s "(+ 1 2)" ls (read-string s)]
+        (println "s type:" (type s))
+        (println "ls type:" (type ls))
+    )
+
+    ;; convert a string to form, than evaluate it
+    (println "Evaluate:" (eval (read-string "(+ 1 2)")))
+)
