ae446edf067ba75483456a4428a25c5a03c6a0c2
Emanuele <egalli64@gmail.com>
2025-04-07 10:38:47 +0200

    defmacro

---
+++ src/p2/ch7/a/e1.clj
@@ -0,0 +1,13 @@
+;;; defmacro
+(ns p2.ch7.a.e1)
+
+;; define a macro
+(defmacro backwards [form] (reverse form))
+
+
+(defn -main []
+    (println
+        ;; invoking the backwars macro
+        ;; that works backwards :O
+        (backwards (" backwards" " am" "I" str)))
+)
