b34054e40786e275b4acf4ff6e3f343ef41c01bb
Emanuele <egalli64@gmail.com>
2025-04-07 16:59:56 +0200

    macroexpand

---
+++ src/p2/ch7/c/e5.clj
@@ -0,0 +1,14 @@
+;;; macroexpand
+;; expand but do not eval
+(ns p2.ch7.c.e5)
+
+;; a macro that discards the last element
+(defmacro ignore-last [funcall] (butlast funcall))
+
+(defn -main []
+    ;; a standard macro
+    (println (macroexpand '(when test (println "hi"))))
+
+    ;; a custom macro, namespace is required
+    (println (macroexpand '(p2.ch7.c.e5/ignore-last (+ 1 2 10))))
+)
