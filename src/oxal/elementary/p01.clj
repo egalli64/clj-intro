f46111523c23e8ef4e90e53e5f1132508e019b29
Emanuele <egalli64@gmail.com>
2025-04-12 10:51:07 +0200

    review

--- src/oxal/elementary/p01.clj
+++ src/oxal/elementary/p01.clj
@@ -1,15 +1,19 @@
 ;; Problem 1, Nothing but the Truth
+;; Fill the __ with a valid element so that expression is true
 ;; https://4clojure.oxal.org/#/problem/1
-;; (= __ true): true
+;; (= __ true)
 
-;; elaborating on the theme
+;; thinking ...
 (println "true is true?" (= true true))
 (println "Is there anything else than the boolean true that is true?" (= 1 true))
 (println (when true "true is truthy"))
 (println (if nil "Unexpected" "nil is falsey"))
 (println (if false "Unexpected" "false is falsey"))
 (println (when (and 0 "" {}) "Anything else is truthy"))
-"I mean, anything else is truthy"
 
-(print "Is the actual problem solved? ")
-(= true true)
+;; my solution
+(def solution true)
+
+;; verify it
+(print "Is the problem solved? ")
+(= solution true)
