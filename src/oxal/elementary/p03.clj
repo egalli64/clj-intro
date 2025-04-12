917da82650105498dd7ebb2520ed1bcf1dd79e0d
Emanuele <egalli64@gmail.com>
2025-04-12 17:55:55 +0200

    review

--- src/oxal/elementary/p03.clj
+++ src/oxal/elementary/p03.clj
@@ -1,12 +1,15 @@
 ;; Problem 3, Strings
 ;; https://4clojure.oxal.org/#/problem/3
-;; (= __ (.toUpperCase "hello world")) : "HELLO WORLD"
+;; (= __ (.toUpperCase "hello world"))
 
-;; elaborating on the theme
+;; thinking ...
 (println "Invoking the Java instance method length on 'Hello':" (.length "Hello"))
 (println "Invoking the Java static method String.valueOf() on Math::sqrt(2):" (String/valueOf (Math/sqrt 2)))
 (println "Instantiate a Java object (explicit new):" (new java.util.Date))
 (println "Instantiate a Java object (implicit new):" (java.util.Date.))
 
-(print "Is the actual problem solved? ")
-(= "HELLO WORLD" (.toUpperCase "hello world"))
+;; my solution
+(def solution "HELLO WORLD")
+
+(print "Is the problem solved? ")
+(= solution (.toUpperCase "hello world"))
