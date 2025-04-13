4080ac5af218c8097ed5aa3db6658193a0c773a2
Emanuele <egalli64@gmail.com>
2025-04-13 12:26:53 +0200

    review

--- src/oxal/easy/p20.clj
+++ src/oxal/easy/p20.clj
@@ -1,14 +1,18 @@
 ;; Problem 20, Penultimate Element
 ;; https://4clojure.oxal.org/#/problem/20
-;; (= (__ (list 1 2 3 4 5)) 4)  : #(last (butlast %))
-;; (= (__ ["a" "b" "c"]) "b")   : #(last (butlast %))
-;; (= (__ [[1 2] [3 4]]) [1 2]) : #(last (butlast %))
+;; (= (__ (list 1 2 3 4 5)) 4)
+;; (= (__ ["a" "b" "c"]) "b")
+;; (= (__ [[1 2] [3 4]]) [1 2])
 
-;; elaborating on the theme
+;; thinking ...
 (println "butlast on seqable - it returns a lazy seq")
 (println "On vector:" (butlast [:a :b :c]))
 (println "For vector the actual type is:" (type (butlast [:a :b])))
 (println "On map:" (butlast {:a 1 :b 2 :c 3}))
+(let [xs [:a :b :c]] (println xs "penultimate is" (last (butlast xs))))
 
-(print "Is the actual problem solved? ")
-(= (#(last (butlast %)) ["a" "b" "c"]) "b")
+;; my solution
+(def solution #(last (butlast %)))
+
+(print "Is the problem solved? ")
+(= (solution ["a" "b" "c"]) "b")
