ddc76a8333c1bcad521142ded32b08d25195ae9e
Emanuele <egalli64@gmail.com>
2025-04-13 18:49:07 +0200

    flatten toy version

--- src/oxal/easy/p28.clj
+++ src/oxal/easy/p28.clj
@@ -1,10 +1,10 @@
 ;; Problem 28, Flatten a Sequence
 ;; https://4clojure.oxal.org/#/problem/28
-;; (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)) : flatten
-;; (= (__ ["a" ["b"] "c"]) '("a" "b" "c"))      : flatten
-;; (= (__ '((((:a))))) '(:a))                   : flatten
+;; (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+;; (= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+;; (= (__ '((((:a))))) '(:a))
 
-;; elaborating on the theme
+;; thinking ...
 (println "flatten works recursively on any sequential")
 (println "On list of vectors:" (flatten '(1 [2 3] [4 [5 6]])))
 ;; beware of quotes!
@@ -12,5 +12,19 @@
 ;; strings are treated naturally since 1.12
 (println "On vector of strings:" (flatten ["Hi" ["Tom", "Bob"]]))
 
-(print "Is the actual problem solved? ")
-(= (flatten '((((:a))))) '(:a))
+(defn my-flatten [x]
+   (if (sequential? x)
+     (mapcat my-flatten x)
+     [x]))
+(println (my-flatten '(1 [2 3] [4 [5 6]])))
+(println (my-flatten (list 1 [2 3] [4 (list 5 6)])))
+(println (my-flatten ["Hi" ["Tom", "Bob"]]))
+
+;; my solution
+(def solution (fn fltn [x]
+   (if (sequential? x)
+     (mapcat fltn x)
+     [x])))
+
+(print "Is the problem solved? ")
+(= (solution '((((:a))))) '(:a))
