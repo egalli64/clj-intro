913b09fca08fc2328f636151cc83e030586919b9
Emanuele <egalli64@gmail.com>
2025-04-14 12:16:21 +0200

    clarify

--- src/brave/p2/ch3/c/e5.clj
+++ src/brave/p2/ch3/c/e5.clj
@@ -8,20 +8,18 @@
 
 ;; my-hello expects a variable (2+) number argument
 (defn my-hello "A variadic function" [a b & more]
-    ;; the first two, mandatory, parameters
-    (print (str "Hello, " a ", " b))
-    ;; if the more list is not empty, print it
-    ;; seq access a collection as a sequence (or nil)
-    ;; commonly used to check if it is not empty
-    (if (seq more) (print ", ... and" more))
-    ;; in any case, EOL
-    (println)
-)
+  ;; the first two parameters are mandatory
+  (print (str "Hello, " a ", " b))
+
+  ;; if the more list is not empty, print it, otherwise just EOL
+  ;; seq access a collection as a sequence (or nil) commonly used to check if it is not empty
+  (if (seq more)
+    (println ", ... and" more)
+    (println)))
 
 (defn -main []
-    (my-hello "Tom" "Bob")
-    (my-hello "Tom" "Bob" "Kim" "Tim")
-    ;; won't work: ArityException (two arguments are mandatory)
-    ;; (my-hello)
-    ;; (my-hello "Tom")
-)
+  (my-hello "Tom" "Bob")
+  (my-hello "Tom" "Bob" "Kim" "Tim"))
+  ;; won't work: ArityException (two arguments are mandatory)
+  ;; (my-hello)
+  ;; (my-hello "Tom")
