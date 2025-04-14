b5c472f7033dc5efaa26a8295734a5959d1b1f52
Emanuele <egalli64@gmail.com>
2025-04-15 00:04:46 +0200

    clarify

--- src/brave/p2/ch6/e2.clj
+++ src/brave/p2/ch6/e2.clj
@@ -8,19 +8,16 @@
 
 (defn -main []
     ;; use ns-interns to get the intern map in a namespace
-    (let [intern-map (ns-interns 'p2.ch6.e2)]
+  (let [intern-map (ns-interns 'brave.p2.ch6.e2)]
         ;; the function get retrieves the value associated to the symbol on that map
         ;; the var reader form, that gives the reference to the object
-        (println "var reader form to 'names':" (get intern-map 'names))
-        (println "... if there is not that symbol:" (get intern-map 'missing))
-    )
+    (println "var reader form to 'names':" (get intern-map 'names))
+    (println "... if there is not that symbol:" (get intern-map 'missing)))
 
     ;; accessing the object from a var reader form
-    (let [vrf (get (ns-interns 'p2.ch6.e2) 'names)]
-        (if (not (nil? vrf))
-            (println "Dereferencing" vrf "gives:" @vrf)
-        )
-    )
+  (let [vrf (get (ns-interns 'brave.p2.ch6.e2) 'names)]
+    (when (not (nil? vrf))
+      (println "Dereferencing" vrf "gives:" @vrf)))
 
     ;; same, but in a simplified way
     ;; if a fully qualified namespace is used (my.ns/symbol), is should match, otherwise Clojure looks:
@@ -28,9 +25,8 @@
     ;; 2) in the namespace loaded by -m
     ;; 3) in imported namespaces, by require or use
     ;; 4) in default namespace (clojure.core)
-    (println "Getting names from the current namespace:" names)
+  (println "Getting names from the current namespace:" names)
 
     ;; names collision
-    (def names "Something unexpected!")
-    (println "What happened to my names?" names)
-)
+  (let [names "Something unexpected!"]
+    (println "What happened to my names?" names)))
