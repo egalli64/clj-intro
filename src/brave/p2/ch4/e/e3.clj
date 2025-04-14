53443fab28606869435a87f8818b60a48d804042
Emanuele <egalli64@gmail.com>
2025-04-14 17:13:18 +0200

    clarify

--- src/brave/p2/ch4/e/e3.clj
+++ src/brave/p2/ch4/e/e3.clj
@@ -4,25 +4,23 @@
 
 ;; generate a function to check if a number is odd
 ;; just negate the standard even? function
-(def odd? (complement even?))
+;; !!! just as example, use the standard odd? instead!
+(def my-odd? (complement even?))
 
 (defn -main []
     ;; plain use of complement
-    (println "is 42 odd?" (odd? 42))
-    (println "is 43 odd?" (odd? 43))
+  (println "is 42 odd?" (my-odd? 42))
+  (println "is 43 odd?" (my-odd? 43))
 
     ;; complement with filter
-    (let [data [1 2 3 4 5 6]]
-        (println "The original data:" data)
-        (println "Filter for even? data:" (filter even? data))
-        (println "Filter for odd? data:" (filter odd? data))
-        (println "Same, inline:" (filter (complement even?) data))
-    )
+  (let [data [1 2 3 4 5 6]]
+    (println "The original data:" data)
+    (println "Filter for even? data:" (filter even? data))
+    (println "Filter for odd? data:" (filter my-odd? data))
+    (println "Same, inline:" (filter (complement even?) data)))
 
     ;; complement to filter out nil
-    (let [data [1 nil nil 4 nil 6]]
-        (println "The original data:" data)
-        (println "Filter for nil? data:" (filter nil? data))
-        (println "Filter for not nil? data:" (filter (complement nil?) data))
-    )
-)
+  (let [data [1 nil nil 4 nil 6]]
+    (println "The original data:" data)
+    (println "Filter for nil? data:" (filter nil? data))
+    (println "Filter for not nil? data:" (filter (complement nil?) data))))
