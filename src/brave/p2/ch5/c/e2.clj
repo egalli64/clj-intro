0dd0f74e60a33ef40bce5fd7df0037be17139540
Emanuele <egalli64@gmail.com>
2025-04-15 00:04:19 +0200

    clarify

--- src/brave/p2/ch5/c/e2.clj
+++ src/brave/p2/ch5/c/e2.clj
@@ -4,20 +4,17 @@
 
 (ns brave.p2.ch5.c.e2)
 
-(defn -main []
-    ;; the actual function
-    (defn square [x]
-        ;; simulation of an expensive operation
-        (println "Called on" x)
-        (* x x)
-    )
-    
-    ;; memoized wrap
-    (def square-memo (memoize square))
+;; the actual function
+(defn square [x]
+  ;; simulation of an expensive operation
+  (println "Called on" x)
+  (* x x))
 
+(defn -main []
+  ;; memoized wrap
+  (let [square-memo (memoize square)]
     (println "Result is:" (square-memo 5))
     (println "Result is:" (square-memo 5))
     (println "Result is:" (square-memo 6))
     (println "Result is:" (square-memo 6))
-    (println "Result is:" (square-memo 5))
-)
+    (println "Result is:" (square-memo 5))))
