5e0a3a0f8fb34d3c6421614fc20caee2045dc5bc
Emanuele <egalli64@gmail.com>
2025-04-15 00:05:47 +0200

    ns directive

--- src/brave/p2/ch6/required.clj
+++ src/brave/p2/ch6/required.clj
@@ -1,9 +1,9 @@
 ;;; a file with functions required elsewhere
 
-(ns brave.p2.ch6.required)
+(ns brave.p2.ch6.required
+  (:require [clojure.string :as str]))
 
 (defn latlon->point "Convert lat/lon map to comma-separated string" [latlon]
-    (str (:lon latlon) "," (:lat latlon))
-)
+  (str (:lon latlon) "," (:lat latlon)))
 
-(defn points [locations] (clojure.string/join " " (map latlon->point locations)))
+(defn points [locations] (str/join " " (map latlon->point locations)))
