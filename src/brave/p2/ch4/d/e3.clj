7c36351f50197591b6c8654b8c3dd2dda68c19b8
Emanuele <egalli64@gmail.com>
2025-04-14 17:09:58 +0200

    clarify

--- src/brave/p2/ch4/d/e3.clj
+++ src/brave/p2/ch4/d/e3.clj
@@ -6,13 +6,13 @@
 
 (ns brave.p2.ch4.d.e3)
 
-(defn -main[]
-    (println "empty seq every? (yes):" (every? #(= % 42) []))
-    (println "vector every? element is 42 (yes):" (every? #(= % 42) [42 42 42]))
-    (println "list every? element is 42 (yes)" (every? #(= % 42) '(42 42 42)))
-    (println "set every? element is even? (yes)" (every? even? #{2 4 6}))
-    (println "map every? value is even?" (every? (fn [[k v]] (even? v)) {:a 2 :b 4 :c 6}))
-    (println "seq every? element is even? (yes)" (every? even? (range 0 10 2)))
-    (println "seq every? element is even? (infinite, no)" (every? even? (range)))
-    (println "nil every? element is 42 (yes!)" (every? even? nil))
-)
+(defn -main []
+  (println "empty seq every? (yes):" (every? #(= % 42) []))
+  (println "vector every? element is 42 (yes):" (every? #(= % 42) [42 42 42]))
+  (println "list every? element is 42 (yes)" (every? #(= % 42) '(42 42 42)))
+  (println "set every? element is even? (yes)" (every? even? #{2 4 6}))
+  ;; the _ signal conventionally a non-used symbol
+  (println "map every? value is even?" (every? (fn [[_ v]] (even? v)) {:a 2 :b 4 :c 6}))
+  (println "seq every? element is even? (yes)" (every? even? (range 0 10 2)))
+  (println "seq every? element is even? (infinite, no)" (every? even? (range)))
+  (println "nil every? element is 42 (yes!)" (every? even? nil)))
