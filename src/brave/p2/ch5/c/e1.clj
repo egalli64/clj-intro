105fa2d377b0cf673692f89ea3fc3e84cf0d4ce3
Emanuele <egalli64@gmail.com>
2025-04-15 00:03:09 +0200

    clarify

--- src/brave/p2/ch5/c/e1.clj
+++ src/brave/p2/ch5/c/e1.clj
@@ -8,17 +8,14 @@
 (ns brave.p2.ch5.c.e1)
 
 (defn -main []
-    ;; three custom functions to be composed
-    (defn increase [x] (+ x 1))
-    (defn square [x] (* x x))
-    (defn dbl [x] (* x 2))
+  ;; three custom functions to be composed
+  (let [increase (fn [x] (+ x 1)), square (fn [x] (* x x)), dbl (fn [x] (* x 2)),
+        ;; compose the three functions above
+        increase-double-square (comp square dbl increase)]
+    (println "3 increased, doubled, squared is" (increase-double-square 3)))
 
-    (def increase-double-square (comp square dbl increase))
-    (println "3 increased, doubled, squared is" (increase-double-square 3))
+  ;; compose anon functions on the fly
+  (println "same (anon function)" ((comp #(* % %) #(* % 2) #(+ % 1)) 3))
 
-    ;; compose anon functions on the fly
-    (println "same (anon function)" ((comp #(* % %) #(* % 2) #(+ % 1)) 3))
-
-    ;; composing standard functions/operators
-    (println "2 * 3 then increased is" ((comp inc *) 2 3))
-)
+  ;; composing standard functions/operators
+  (println "2 * 3 then increased is" ((comp inc *) 2 3)))
