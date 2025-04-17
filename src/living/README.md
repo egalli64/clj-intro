5c01bfa8702ab69afea2349a7a8ea74593a476a6
Emanuele <egalli64@gmail.com>
2025-04-17 19:48:32 +0200

    numbers

---
+++ src/living/README.md
@@ -0,0 +1,12 @@
+# clj-intro
+## Introduction to Clojure
+
+Tested on Java 21, Clojure 1.12
+
+Notes jotted down while reading Living Clojure by Carin Meier (2015) O'Reilly
+https://www.oreilly.com/library/view/living-clojure/9781491909270/
+
+## Part 1 - A Guided Tour of Clojure
+### Chapter 1 - The Structure of Clojure
+#### a. Baby Steps with Simple Values
+    e1. numbers
---
+++ src/living/p1/ch1/a/e1.clj
@@ -0,0 +1,27 @@
+;;; For more info read Living Clojure by Carin Meier
+;;
+;; Run it in the REPL, for insstance by: (load-file "src/living/p1/ch1/a/e1.clj")
+
+;; an integer as literal
+(println 42)
+
+;; a real number
+(println 42.23)
+
+;; a ratio
+(println 3/7)
+
+;; a ratio, reduced to integer
+(println 35/7)
+
+;; a ratio, simplified
+(println 7/35)
+
+;; an invalid number
+;; (println 4.0/2)
+
+;; an expression leading to a ratio
+(let [x (/ 14 35)] (println x))
+
+;; (almost) same expression leading to a real number
+(let [x (/ 14 35.0)] (println x))
