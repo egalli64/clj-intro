d6fd92b69149667d20992121070f64a0b6a382e2
Emanuele <egalli64@gmail.com>
2025-05-08 19:46:52 +0200

    cons

---
+++ src/aaa/README.md
@@ -0,0 +1,7 @@
+# clj-intro
+## Introduction to Clojure
+
+Tested on Java 21, Clojure 1.12
+
+## Some commonly used Clojure functions 
+- [cons](cons.clj)
---
+++ src/aaa/cons.clj
@@ -0,0 +1,8 @@
+;; clj-intro - Introduction to Clojure
+;; https://github.com/egalli64/clj-intro
+;; cons
+
+; construct a new seq prepending a new element to the passed one
+(cons 1 [2 3 4])                                            ; (1 2 3 4)
+; pass nil to create a new list containing just the passed value
+(cons 1 nil)                                                ; (1)
