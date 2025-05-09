85fa621fe40b1c5f0b091b143e7972ebc768dd0c
Emanuele <egalli64@gmail.com>
2025-05-09 09:34:23 +0200

    clarify

--- src/aaa/conj.clj
+++ src/aaa/conj.clj
@@ -1,12 +1,16 @@
 ;; clj-intro - Introduction to Clojure
 ;; https://github.com/egalli64/clj-intro
-;; conj
+;; (conj collection value)
 
 ; conjunct a new value in the most efficient way for that sequence
 ; preserving the collection type
 (conj [1 2 3] 4)                                            ; [1 2 3 4]
 (conj '(1 2 3) 0)                                           ; (0 1 2 3)
+; nil is considered an empty list
+(conj nil 1)                                                ; (1)
+
+; create an empty vector
+(conj)                                                      ; []
+
 ; accepts multiple arguments
 (conj #{1 2 3} 4 5 6)                                       ; #{1 4 6 3 2 5}
-; ClassCastException
-; (conj 1 nil)
