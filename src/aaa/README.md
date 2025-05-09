340cbed740867e53e7a0caf0202d5e7d15dd209b
Emanuele <egalli64@gmail.com>
2025-05-09 09:51:02 +0200

    map

--- src/aaa/README.md
+++ src/aaa/README.md
@@ -4,5 +4,6 @@
 Tested on Java 21, Clojure 1.12
 
 ## Some commonly used Clojure functions 
-- [cons](cons.clj) - prepend a new item
-- [conj](conj.clj) - add one or more items
+- [cons](cons.clj) - construct a seq 
+- [conj](conj.clj) - conjunct items to a collection
+- [map](map.clj) - transform items in a collection
---
+++ src/aaa/map.clj
@@ -0,0 +1,14 @@
+;; clj-intro - Introduction to Clojure
+;; https://github.com/egalli64/clj-intro
+;; (map function collection)
+
+;; generate a lazy seq applying a function to each item in the collection
+(map #(+ % 1) [1 2 3 4])                                    ; (2 3 4 5)
+
+(map clojure.string/capitalize ["bob" "tom"])               ; ("Bob" "Tom")
+
+;; map more collections
+(map + [1 2 3] [4 5 6])                                     ; (5 7 9)
+(map + [1 2 3] [4 5])                                       ; (5 7)
+;; using vector zip the passed vectors in the result
+(map vector [:a :b :c] [1 2 3])                             ; ([:a 1] [:b 2] [:c 3])
