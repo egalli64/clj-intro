52bb46a474adaf3c9f83c81dd3e0669f261026ee
Emanuele <egalli64@gmail.com>
2025-05-09 09:36:44 +0200

    clarify

--- src/aaa/cons.clj
+++ src/aaa/cons.clj
@@ -1,6 +1,6 @@
 ;; clj-intro - Introduction to Clojure
 ;; https://github.com/egalli64/clj-intro
-;; cons
+;; (cons value sequence)
 
 ; construct a new seq prepending a new element to the passed one
 (cons 1 [2 3 4])                                            ; (1 2 3 4)
