476fff07f7ce3868725c7610b19e91e34f59a9e9
Emanuele <egalli64@gmail.com>
2025-05-12 11:27:50 +0200

    clarify order preservation

--- src/getting/p1/ch3/a/e1.clj
+++ src/getting/p1/ch3/a/e1.clj
@@ -6,6 +6,7 @@
 
 ;; literal syntax to define a map
 (def book {"title" "Oliver Twist", "author" "Dickens", "published" 1838})
+;; notice that the entry insertion order is preserved
 (println "The book map:" book)
 
 ;; using the hash-map function
