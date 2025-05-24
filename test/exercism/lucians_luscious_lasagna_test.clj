620e978aadb80aaf7a68c2d3549f72399b5b1b73
Emanuele <egalli64@gmail.com>
2025-05-24 19:54:25 +0200

    readability

--- test/exercism/lucians_luscious_lasagna_test.clj
+++ test/exercism/lucians_luscious_lasagna_test.clj
@@ -7,18 +7,16 @@
 
 (ns exercism.lucians-luscious-lasagna_test
   (:require [clojure.test :refer :all])
-  (:require [exercism.lucians-luscious-lasagna :refer [expected-time
-                                                       remaining-time
-                                                       prep-time
-                                                       total-time
-                                                       ]]))
+  (:require [exercism.lucians-luscious-lasagna :refer [expected-time remaining-time prep-time total-time]]))
 
 (deftest test-expected-time (is (= 40 expected-time)))
 
 (deftest test-remaining-time (is (= 15 (remaining-time 25))))
 
-(deftest test-prep-time-single (is (= 2 (prep-time 1))))
-(deftest test-prep-time-multi (is (= 8 (prep-time 4))))
+(deftest test-prep-time
+  (testing "Prep Time - Single Layer" (is (= 2 (prep-time 1))))
+  (testing "Prep Time - Multi Layer" (is (= 8 (prep-time 4)))))
 
-(deftest test-total-time-single (is (= 32 (total-time 1 30))))
-(deftest test-total-time-multi (is (= 16 (total-time 4 8))))
+(deftest test-total-time
+  (testing "Total Time - Single Layer" (is (= 32 (total-time 1 30))))
+  (testing "Total Time - Multi Layer" (is (= 16 (total-time 4 8)))))
