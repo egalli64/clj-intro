9c3436e83ef3fc6e993461de0e8cd874a8812969
Emanuele <egalli64@gmail.com>
2025-07-13 12:54:41 +0200

    keyword

---
+++ src/aaa/literal_keyword.clj
@@ -0,0 +1,28 @@
+;; clj-intro - Introduction to Clojure
+;; https://github.com/egalli64/clj-intro
+;; Keyword
+
+;; a literal Keyword
+(type :keyword)
+(println :keyword)
+
+;; same, but using the constructor function keyword
+(type (keyword "keyword"))
+(keyword "keyword")
+
+;; idiomatically used as key in a map
+(def my-map {:a 1, :b 2, :c 3})
+(println my-map)
+
+;;
+;; keywords can be namespaced - usually not Clojure namespace, they are meant to better describe the keyword use
+(println :product/id)
+(println :account/id)
+
+;;
+;; special syntax to put a keyword in the current namespace
+(println ::id)
+
+;; assuming we are in the :user namespace
+(let [id-1 ::id, id-2 :user/id]
+  (identical? id-1 id-2))
