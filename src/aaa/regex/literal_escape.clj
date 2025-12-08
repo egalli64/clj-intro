b21479b81310dd8a1874375009d847272321fc17
Emanuele <egalli64@gmail.com>
2025-12-08 16:20:16 +0100

    regex pattern anchors: escape on \ and |

--- src/aaa/regex/literal_escape.clj
+++ src/aaa/regex/literal_escape.clj
@@ -3,26 +3,30 @@
 ;; Regular expression pattern literal with escaped characters
 ;; Backslash is required for: . * + ? ^ $ \ | ( ) [ ] { }
 
-# \. \*
+;; \. \*
 (let [pattern #"a\.b\*"]
   (re-find pattern "a.b*"))
 
-# \+ \?
+;; \+ \?
 (let [pattern #"a\+b\?"]
   (re-find pattern "a+b?"))
 
-# \^ \$
+;; \^ \$
 (let [pattern #"a\^b\$"]
   (re-find pattern "a^b$"))
 
-# \( \)
+;; \\ \| - notice that in the string the backslash character has to be escaped too!
+(let [pattern #"\\a\|b"]
+  (re-find pattern "\\a|b"))
+
+;; \( \)
 (let [pattern #"a\(b\)"]
   (re-find pattern "a(b)"))
 
-# \[ \]
+;; \[ \]
 (let [pattern #"a\[b\]"]
   (re-find pattern "a[b]"))
 
-# \{ \}
+;; \{ \}
 (let [pattern #"a\{b\}"]
   (re-find pattern "a{b}"))
