5eceea273a4c80030d7fff4f0d5ab8cc76cec2a0
Emanuele <egalli64@gmail.com>
2025-12-08 16:27:13 +0100

    regex pattern anchors: single \b

--- src/aaa/regex/anchor_word.clj
+++ src/aaa/regex/anchor_word.clj
@@ -10,7 +10,20 @@
   (println (re-find pattern "hello, Tom!"))
   (println (re-find pattern "My name is Bob Thellof")))
 
+;; match word starting by "cat"
+(let [pattern #"\bcat"]
+  (println (re-find pattern "That's a sleepy cat!"))
+  (println (re-find pattern "That's a huge catfish!"))
+  (println (re-find pattern "We need the bobcat, now!")))
+
+;; match word ending by "cat"
+(let [pattern #"cat\b"]
+  (println (re-find pattern "That's a sleepy cat!"))
+  (println (re-find pattern "That's a huge catfish!"))
+  (println (re-find pattern "We need the bobcat, now!")))
+
 ;; \B is the opposite of \b, not a word boundary - match inside word
 (let [pattern #"\Bhello\B"]
   (println (re-find pattern "hello, Tom!"))
   (println (re-find pattern "My name is Bob Thellof")))
+
